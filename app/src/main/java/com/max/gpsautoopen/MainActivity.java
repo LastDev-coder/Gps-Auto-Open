08a57692d0a88772e67ebeeba606f8610e9eb1f4
LastDev-coder <iamatanu.cs@gmail.com>
2021-05-21 11:43:22 +0530

    gps auto open done

--- app/src/main/java/com/max/gpsautoopen/MainActivity.java
+++ app/src/main/java/com/max/gpsautoopen/MainActivity.java
@@ -1,16 +1,14 @@
 package com.max.gpsautoopen;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
@@ -21,91 +19,67 @@ import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.gun0912.tedpermission.PermissionListener;
-import com.gun0912.tedpermission.TedPermission;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
-    int REQUEST_LOCATION = 88;
+    int REQUEST_LOCATION_setting = 1234;
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
-        getPermission();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                locationRequest = LocationRequest.create();
+                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                locationRequest.setInterval(5000);
+                locationRequest.setFastestInterval(2000);
 
-                        Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
-                    Location();
-                }else {
-                    // when permission denied
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{
-                             Manifest.permission.ACCESS_FINE_LOCATION
-                            }, REQUEST_LOCATION
-                            );
-                }
-            }
-        });
-    }
 
-    private void Location() {
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(5000);
-        locationRequest.setFastestInterval(2000);
+                LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                        .addLocationRequest(locationRequest);
+                builder.setAlwaysShow(true);
 
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-        builder.setAlwaysShow(true);
 
-        Task<LocationSettingsResponse> responseTask = LocationServices.getSettingsClient(getApplicationContext())
-                .checkLocationSettings(builder.build());
-        responseTask.addOnCompleteListener(task -> {
-            try {
-                LocationSettingsResponse response = task.getResult(ApiException.class);
-            }catch (ApiException e){
-                switch (e.getStatusCode()){
-                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                Task<LocationSettingsResponse> requestTask = LocationServices.getSettingsClient(getApplicationContext())
+                        .checkLocationSettings(builder.build());
+
+                requestTask.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onComplete(Task<LocationSettingsResponse> task) {
                         try {
-                            ResolvableApiException resolvableApiException  =(ResolvableApiException) e;
-                            resolvableApiException.startResolutionForResult(MainActivity.this,REQUEST_LOCATION);
+                            LocationSettingsResponse response = task.getResult(ApiException.class);
+                            Toast.makeText(MainActivity.this, "Gps already turned on", Toast.LENGTH_SHORT).show();
+                        } catch (ApiException e) {
+                            e.printStackTrace();
 
-                        }catch (Exception k){
-                            k.printStackTrace();
+                            switch (e.getStatusCode()) {
+                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                    ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                    try {
+                                        resolvableApiException.startResolutionForResult(MainActivity.this, REQUEST_LOCATION_setting);
+                                    } catch (IntentSender.SendIntentException sendIntentException) {
+                                        sendIntentException.printStackTrace();
+                                    }
+                                    break;
+
+
+                                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                    break;
+                            }
                         }
-                    break;
-                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                        break;
-                }
+                    }
+                });
+
             }
+
+
         });
     }
 
-    private void getPermission() {
-        PermissionListener permissionlistener = new PermissionListener() {
-            @Override
-            public void onPermissionGranted() {
-                Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onPermissionDenied(List<String> deniedPermissions) {
-                Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
-            }
-        };
-
-        TedPermission.with(this)
-                .setPermissionListener(permissionlistener)
-                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
-                .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
-                .check();
-    }
 }
